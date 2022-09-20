1c8f63d49a9d1b68197c617aae2d9e1051556475
Enrique <0208738@up.edu.mx>
2022-09-19 20:58:31 -0500

    Subi el primer examen parcial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+
+  const [datos, setDatos] = useState([]);
+  const [nombre, setNombre] = useState('');
+  const [apellido, setApellido] = useState('');
+  const [apodo, setApodo] = useState('');
+
+
+  const isDisable = (nombre === '' || apellido === '' || apodo === '');
+
+  const cancelCourse = () => {
+    setNombre('')
+    setApellido('')
+    setApodo('')
+  }
+
+  const EnviarDatos = () => {
+    const nuevosDatos = {
+      nombre: nombre,
+      apellido: apellido,
+      apodo: apodo
+    }
+    setDatos([nuevosDatos,...datos])
+  }
+
+  const Imprimir = () => {
+    return datos.map((dato) => (
+      <ls key={dato}>
+        {dato.nombre}, 
+        {dato.apellido}, 
+        [{dato.apodo}]
+      </ls>
+    ))
+  }
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>Nombre:</label>
+      <input 
+        type = "text"
+        value={nombre}
+        onChange={(e) => {setNombre(e.target.value)}}>
+      </input>
+      <br/>
+
+      <label>Apellido:</label>
+      <input 
+        type = "text"
+        value={apellido}
+        onChange={(e) => {setApellido(e.target.value)}}>
+      </input>
+      <br/>
+
+      <label>Apodo:</label>
+      <input 
+        type = "text"
+        value={apodo}
+        onChange={(e) => {setApodo(e.target.value)}}>
+      </input>
+      <br/>
+      <button disabled={isDisable} onClick={EnviarDatos}>Enviar</button>
+      <button onClick={cancelCourse}>Limpiar</button>
+
+      <br/>
+      {Imprimir()}
     </div>
+    
+    
   );
 }
 
